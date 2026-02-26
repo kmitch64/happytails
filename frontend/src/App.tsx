2af3d2fc8d496a942bad7292307d999b9f3dec1a
Goodsie <gidsola@users.noreply.github.com>
2026-02-25 19:43:49 -0500

    Update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import type { JSX } from 'react'; // importing only JSX type for better performance and to avoid importing the entire React library
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom'; 
 
 import Home from "./pages/Home/Home";
 import Adopt from './pages/Adopt/Adopt';
