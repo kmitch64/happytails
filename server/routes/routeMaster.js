7646b4ea87c3191f41c43215505fe364cf03c48c
Goodsie <gidsola@users.noreply.github.com>
2026-03-16 14:31:12 -0400

    Update health check route to remove API prefix

--- server/routes/routeMaster.js
+++ server/routes/routeMaster.js
@@ -5,7 +5,7 @@ import express from 'express';
 //API Routes (Json)
 export default async function RouteMaster(/**@type {express.Express} */app) {
     app
-        .get('/api/health', (_, res) => {
+        .get('/health', (_, res) => {
             res.status(200).json({ status: 'OK' });
         });
 };
