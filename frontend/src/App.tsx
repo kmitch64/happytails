888c45f41ddd1b72a9f8853301d82f0c5e2b619c
Goodsie <gidsola@users.noreply.github.com>
2026-03-21 22:04:20 -0400

    refactor(App): clean up imports and improve readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-/* import React from 'react'; */ // importing full React to access React.JSX.Element type
-import type { JSX } from 'react'; // importing only JSX type for better performance and to avoid importing the entire React library
 
+import type { JSX } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { Link } from 'react-router-dom'; 
 
-import Home from "./pages/Home/Home";
 import AuthProvider from './components/auth/AuthProvider';
 // import ProtectedRoute from './components/auth/ProtectedRoute';
 
+//layout
+import DefaultLayout from './components/layouts/DefaultLayout';
 
 //auth pages
+// Frontend peeps can decide if these go in the layout or not, currently inside.
 import Login from './pages/login/login';
 import Register from './pages/register/register';
 import Logout from './pages/logout/logout';
 import TwoFactorAuth from './pages/2fa/2fa';
 
+//home page
+import Home from "./pages/Home/Home";
+
 //adopt pages
 import Adopt from './pages/Adopt/Adopt';
 import AdoptableProfile from './pages/Adopt/AdoptableProfile';
 import BrowseAdoptables from './pages/Adopt/BrowseAdoptables';
-import DefaultLayout from './components/layouts/DefaultLayout';
 
+
+//global styles
 import './App.css';
 
 export default function App(): JSX.Element {
@@ -28,6 +32,7 @@ export default function App(): JSX.Element {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+
           <Route element={<DefaultLayout />}>
 
             <Route path='/login' element={<Login />} />
@@ -39,8 +44,9 @@ export default function App(): JSX.Element {
             <Route path='/adopt' element={<Adopt />} />
             <Route path='/adopt/profile' element={<AdoptableProfile />} />
             <Route path='/adopt/browse' element={<BrowseAdoptables />} />
-            
+
           </Route>
+
         </Routes>
       </BrowserRouter>
     </AuthProvider>
