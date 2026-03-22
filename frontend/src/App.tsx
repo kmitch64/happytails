9cbe2a4f55930f4ad56c37612c037b65e35be7b8
Goodsie <gidsola@users.noreply.github.com>
2026-03-22 17:43:29 -0400

    ext removal in tsx for cloud build(wtf)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import type { JSX } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import AuthProvider from './components/auth/AuthProvider.jsx';
+import AuthProvider from './components/auth/AuthProvider';
 // import ProtectedRoute from './Components/auth/ProtectedRoute';
 
 //layout
@@ -10,18 +10,18 @@ import DefaultLayout from './components/layouts/DefaultLayout';
 
 //auth pages
 // Frontend peeps can decide if these go in the layout or not, currently inside.
-import Login from './pages/login/login.jsx';
-import Register from './pages/register/register.jsx';
-import Logout from './pages/logout/logout.jsx';
-import TwoFactorAuth from './pages/2fa/2fa.jsx';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
+import Logout from './pages/logout/logout';
+import TwoFactorAuth from './pages/2fa/2fa';
 
 //home page
-import Home from "./pages/Home/Home.jsx";
+import Home from './pages/Home/Home';
 
 //adopt pages
 import Adopt from './pages/Adopt/Adopt.jsx';
-import AdoptableProfile from './pages/Adopt/AdoptableProfile.jsx';
-import BrowseAdoptables from './pages/Adopt/BrowseAdoptables.jsx';
+import AdoptableProfile from './pages/Adopt/AdoptableProfile';
+import BrowseAdoptables from './pages/Adopt/BrowseAdoptables';
 
 
 //global styles
