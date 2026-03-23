5a64f3f82f2eae08dc5fbf627711b6845b8464ef
Goodsie <gidsola@users.noreply.github.com>
2026-03-22 20:51:02 -0400

    import

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../components/auth/AuthContext.js';
+import { useAuth } from '../../components/auth/AuthContext';
 // import MetaData from '../../components/metadata/MetaData.jsx';
 
 import './login.css';
