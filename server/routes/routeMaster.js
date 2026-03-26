6c701c1caff0f2f98319b6268471d3a437c02744
Goodsie <gidsola@users.noreply.github.com>
2026-03-26 10:41:13 -0400

    refactor: remove image upload routes and controller

--- server/routes/routeMaster.js
+++ server/routes/routeMaster.js
@@ -3,7 +3,6 @@ import express from 'express';
 
 import authRouter from './v1/auth.js';
 import userRouter from './v1/user.js';
-import imageRouter from './v1/image.routes.js';
 import petRouter from './v1/pet.routes.js';
 import adoptionRouter from './v1/adoption.routes.js';
 import sitterRouter from './v1/sitter.routes.js';
@@ -14,7 +13,6 @@ export default async function RouteMaster(/**@type {express.Express} */app) {
   app
     .use('/api/v1/auth', authRouter)
     .use('/api/v1/users', userRouter)
-    .use('/api/v1/images', imageRouter)
     .use('/api/v1/pets', petRouter)
     .use('/api/v1/adoptions', adoptionRouter)
     .use('/api/v1/sitters', sitterRouter)
